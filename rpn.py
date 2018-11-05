b71a8778f4dfc1e489f6fac85151deca2e6b09b0
Joyce Sin Jie Yin <Joyce@Joyces-MacBook-Pro.local>
2018-11-05 00:36:40 -0500

    Added rpn support for exponent

--- rpn.py
+++ rpn.py
@@ -15,7 +15,10 @@ def calculate(arg):
 				result = val1 + val2
 			elif token == '-':
 				result = val1 - val2
-
+			elif token == '^':
+				result = 1
+				for x in val2:
+					result = result * val1 
 
 			stack.append(result)
 	if len(stack) > 1:
